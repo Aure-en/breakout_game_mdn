55a3da67a8cf85264316ad5adcd74727a01a42fd
Aurelie <nn.aurelie@hotmail.com>
2021-09-25 21:44:34 +0200

    Learnt how to move elements on <canvas>

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 // Select <canvas> element
 const canvas = document.querySelector("#canvas");
-
-// Create the "2D rendering context" = Tool used to draw on <canvas>
 const ctx = canvas.getContext("2d");
 
-// Create a red square whose left corner position is { x: 20px, y: 40px }.
-// All instructions always start with beginPath and end with closePath.
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+let x = canvas.width / 2;
+let y = canvas.height - 30;
 
-// Draw a green circle
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+const dx = 2;
+const dy = -2;
 
-// Stroke instead of fill to only color the outline.
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  x += dx;
+  y += dy;
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+setInterval(draw, 10);
