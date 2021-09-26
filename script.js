effc674c40916d1b42a1105db38f9a1ed9bbcda0
Aurelie <nn.aurelie@hotmail.com>
2021-09-26 09:58:24 +0200

    Learnt how to add player controls

--- script.js
+++ script.js
@@ -2,26 +2,70 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+// Ball
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-let color = 'blue';
 const ballRadius = 10;
 
+// Paddle
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// Ball movements
 let dx = 2;
 let dy = -2;
 
+// Player controls
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = true;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === 'ArrowRight') {
+    rightPressed = false;
+  } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+    leftPressed = false;
+  }
+}
+
+// Draw
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
+  // Ball coordinates
   if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
     dy = -dy;
-    color = 'red';
   }
 
   if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
     dx = -dx;
-    color = 'green';
+  }
+
+  // Paddle coordinates
+  if (rightPressed) {
+    paddleX += 7;
+    if (paddleX + paddleWidth > canvas.width) {
+      paddleX = canvas.width - paddleWidth
+    }
+
+  } else if (leftPressed) {
+    paddleX -= 7;
+    if (paddleX < 0) {
+      paddleX = 0;
+    }
   }
 
   x += dx;
@@ -31,7 +75,15 @@ function draw() {
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = color;
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
 }
