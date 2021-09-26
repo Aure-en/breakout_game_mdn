bc228f31571397552dbfdf520aa70478db2cfc70
Aurelie <nn.aurelie@hotmail.com>
2021-09-26 09:45:26 +0200

    Learnt how to keep moving elements within canvas boundaries.

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ const ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+let color = 'blue';
+const ballRadius = 10;
 
-const dx = 2;
-const dy = -2;
+let dx = 2;
+let dy = -2;
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+
+  if(y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy;
+    color = 'red';
+  }
+
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx;
+    color = 'green';
+  }
+
   x += dx;
   y += dy;
 }
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+  ctx.fillStyle = color;
   ctx.fill();
   ctx.closePath();
 }
